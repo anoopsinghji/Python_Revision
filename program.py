1daf488ac8055e089c20e3bfafa81e3aa70cea94
MediGuide Developer <mediaguide@example.com>
2026-05-28 11:04:23 +0530

    another program

---
+++ program.py
@@ -0,0 +1,4 @@
+#PROGRAM 2.7   Write a program to demonstrate the use of int before input.
+Num1 = int(input(' Please Enter Number:')) 
+print(' Num1 = ',Num1) #Print the value of Num1
+print(type(Num1)) #Check type of Num1 
