afa4d0955ada136eca566f31f467fcf2bd791e1b
MediGuide Developer <mediaguide@example.com>
2026-05-28 10:48:21 +0530

    started

---
+++ hello.py
@@ -0,0 +1,50 @@
+
+# Write a program to display the messages “Hello”, “World” and “Good Bye”. Each of the three 
+# messages should get displayed on a different line.
+print("Hello")
+print("World")
+print("Good Bye")
+
+ #Write a basic program to make use of the end key and display the messages “Hello” “World” 
+#and “Good Bye” in one line
+print('hello', end=' ')
+print('world', end=' ')
+print('good bye')
+
+
+#Write a program to calculate the area of a rectangle.
+length = 10
+breadth = 5
+
+area = length * breadth
+
+print('Length =', length, 'Breadth =', breadth)
+
+print ('The area of the rectangle is', area)
+
+#Write a program to read strings from the keyboard
+
+String1= input('Enter a string: ')
+String2 = input('Enter another string: ')
+
+print('String1 =', String1,'String2 =', String2)
+
+#PROGRAM 2.5 Write a program to enter digits instead of characters.
+print('Enter a digit: ')
+x = input()
+print('You entered:', x)
+print('The type of the input is:')
+print(type(x))
+
+#PROGRAM 2.6   Write a program to demonstrate the use of int and input function. 
+
+print('Please Enter Number') 
+Num1 = input() #Get input from user
+print('Num1 = ',Num1) #Print value of Num1
+print(type(Num1)) #Check type of Num1
+print('Converting type of Num1 to int')
+Num1 = int(Num1) #Convert type of Num1 from str to int 
+print(Num1) #print the value of Num1 
+print(type(Num1))
+
+
